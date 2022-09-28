8396d0419a2730e01d5e676edc1ba57d8d741dd8
aniakucherenko <ania.kucherenko@gmail.com>
2022-09-28 17:40:04 +0300

    update project

---
+++ dist/index.9394b74e.js
@@ -0,0 +1,14 @@
+(()=>{
+    var swiper = new Swiper(".mySwiper", {
+        loop: true,
+        autoplay: true
+    });
+    var swiper = new Swiper(".customersSwiper", {
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        }
+    });
+})();
+
+//# sourceMappingURL=index.9394b74e.js.map
